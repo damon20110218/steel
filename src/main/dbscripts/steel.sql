5371c9b46d231f3117763c8f8258a278cc046ffe
damon20110218 <damon_zhaojin@163.com>
2019-06-25 09:48:38 +0800

    新增steel_future_price表

--- src/main/dbscripts/steel.sql
+++ src/main/dbscripts/steel.sql
@@ -1,22 +1,30 @@
 DROP TABLE client_info;
-CREATE TABLE client_info (client_id int NOT NULL AUTO_INCREMENT, client_name varchar(400), create_date date, client_type varchar(8) COMMENT '1- ¹«Ë¾ 2-¿Í»§', client_mark varchar(500), mobile varchar(100), telephone varchar(100), email varchar(100), address varchar(300), contact_person varchar(45), PRIMARY KEY (client_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_ci COMMENT='¿Í»§ÐÅÏ¢±í';
+CREATE TABLE client_info (client_id int NOT NULL AUTO_INCREMENT, client_name varchar(400), create_date date, client_type varchar(8) COMMENT '1- ï¿½ï¿½Ë¾ 2-ï¿½Í»ï¿½', client_mark varchar(500), mobile varchar(100), telephone varchar(100), email varchar(100), address varchar(300), contact_person varchar(45), PRIMARY KEY (client_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_ci COMMENT='ï¿½Í»ï¿½ï¿½ï¿½Ï¢ï¿½ï¿½';
 DROP TABLE steel_category;
-CREATE TABLE steel_category (category_id int NOT NULL, steel_name varchar(200), steel_en_name varchar(200), steel_code varchar(45), length int, width int, calc_type varchar(45), PRIMARY KEY (category_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_ci COMMENT='¸Ö°åÖÖÀà±í£¬Î¬»¤¸Ö°åÃû³Æ£¬³¤¶ÈÓë¿í¶È';
+CREATE TABLE steel_category (category_id int NOT NULL, steel_name varchar(200), steel_en_name varchar(200), steel_code varchar(45), length int, width int, calc_type varchar(45), PRIMARY KEY (category_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_ci COMMENT='ï¿½Ö°ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Î¬ï¿½ï¿½ï¿½Ö°ï¿½ï¿½ï¿½ï¿½Æ£ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½';
 DROP TABLE steel_inventory;
 CREATE TABLE steel_inventory (inventory_id int NOT NULL AUTO_INCREMENT, spec_id varchar(45), inventory_date varchar(45), store_in double(10,4), store_out double(10,4), year int, month int, PRIMARY KEY (inventory_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_ci;
 DROP TABLE steel_order;
-CREATE TABLE steel_order (order_id int NOT NULL AUTO_INCREMENT, order_date date, order_no varchar(45), client_id int, client_no varchar(45), spec_id int, client_amount double(10,4), price double(10,4), steel_calc_amount varchar(100), comment varchar(400), is_out int(2) COMMENT '0-Î´³ö¿â  1-ÒÑ³ö¿â', is_sale int(2) COMMENT '0-Î´ÏúÊÛ  1-ÒÑÏúÊÛ', is_delete int(2), unit varchar(45), year int(4), month int(2), PRIMARY KEY (order_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_ci COMMENT='¸Ö°å¶©µ¥±í';
+CREATE TABLE steel_order (order_id int NOT NULL AUTO_INCREMENT, order_date date, order_no varchar(45), client_id int, client_no varchar(45), spec_id int, client_amount double(10,4), price double(10,4), steel_calc_amount varchar(100), comment varchar(400), is_out int(2) COMMENT '0-Î´ï¿½ï¿½ï¿½ï¿½  1-ï¿½Ñ³ï¿½ï¿½ï¿½', is_sale int(2) COMMENT '0-Î´ï¿½ï¿½ï¿½ï¿½  1-ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½', is_delete int(2), unit varchar(45), year int(4), month int(2), PRIMARY KEY (order_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_ci COMMENT='ï¿½Ö°å¶©ï¿½ï¿½ï¿½ï¿½';
 DROP TABLE steel_outbound;
 CREATE TABLE steel_outbound (out_id int NOT NULL AUTO_INCREMENT, order_no varchar(45), spec_id int, actual_amount double(10,4), out_date date, year int, month int, PRIMARY KEY (out_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_ci;
 DROP TABLE steel_parameter;
-CREATE TABLE steel_parameter (param_id int NOT NULL, density double(10,4), category_id int, PRIMARY KEY (param_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_ci COMMENT='¸Ö°å»ù±¾ÐÅÏ¢±í£¬Î¬»¤ÃÜ¶ÈµÈÐÅÏ¢';
+CREATE TABLE steel_parameter (param_id int NOT NULL, density double(10,4), category_id int, PRIMARY KEY (param_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_ci COMMENT='ï¿½Ö°ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ï¢ï¿½ï¿½Î¬ï¿½ï¿½ï¿½Ü¶Èµï¿½ï¿½ï¿½Ï¢';
 DROP TABLE steel_price;
-CREATE TABLE steel_price (price_id int NOT NULL AUTO_INCREMENT, price_code varchar(45), price double(10,4), price_date date, price_type int(2) COMMENT '1-ÆÚ»õ¼Û¸ñ 2-ÁãÊÛ¼Û¸ñ', PRIMARY KEY (price_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_ci COMMENT='¸Ö°åÃ¿ÈÕ¼Û¸ñ±í';
+CREATE TABLE steel_price (price_id int NOT NULL AUTO_INCREMENT, price_code varchar(45), price double(10,4), price_date date, price_type int(2) COMMENT '1-ï¿½Ú»ï¿½ï¿½Û¸ï¿½ 2-ï¿½ï¿½ï¿½Û¼Û¸ï¿½', PRIMARY KEY (price_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_ci COMMENT='ï¿½Ö°ï¿½Ã¿ï¿½Õ¼Û¸ï¿½ï¿½';
 DROP TABLE steel_sale;
 CREATE TABLE steel_sale (sale_id int NOT NULL AUTO_INCREMENT, sale_no varchar(45), order_no varchar(45), sale_date date, year int, month int, sale_amout double(10,4), cash_amount double(10,4), PRIMARY KEY (sale_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_ci;
 DROP TABLE steel_specs;
-CREATE TABLE steel_specs (spec_id int NOT NULL, thickness double(10,3), steel_code varchar(45), spec_desc varchar(300), category_id int, price_code varchar(45), PRIMARY KEY (spec_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_ci COMMENT='¸Ö°å¹æ¸ñÐÅÏ¢£¬Ö÷ÒªÎ¬»¤¸Ö°åºñ¶È';
+CREATE TABLE steel_specs (spec_id int NOT NULL, thickness double(10,3), steel_code varchar(45), spec_desc varchar(300), category_id int, price_code varchar(45), PRIMARY KEY (spec_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_ci COMMENT='ï¿½Ö°ï¿½ï¿½ï¿½ï¿½ï¿½Ï¢ï¿½ï¿½ï¿½ï¿½ÒªÎ¬ï¿½ï¿½ï¿½Ö°ï¿½ï¿½ï¿½';
 DROP TABLE steel_storage;
-CREATE TABLE steel_storage (storage_id int NOT NULL AUTO_INCREMENT, store_date date, client_no varchar(100), store_no varchar(100), steel_amount double(10,4), steel_factory varchar(200), year int, month int, spec_id int, client_id int, cash_amount double(10,2), price double(10,4), PRIMARY KEY (storage_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_ci COMMENT='¹¤³§¸Ö°åÈë¿âÐÅÏ¢±í';
+CREATE TABLE steel_storage (storage_id int NOT NULL AUTO_INCREMENT, store_date date, client_no varchar(100), store_no varchar(100), steel_amount double(10,4), steel_factory varchar(200), year int, month int, spec_id int, client_id int, cash_amount double(10,2), price double(10,4), PRIMARY KEY (storage_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_ci COMMENT='ï¿½ï¿½ï¿½ï¿½ï¿½Ö°ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ï¢ï¿½ï¿½';
 DROP TABLE user;
 CREATE TABLE user (user_id int NOT NULL, user_name varchar(45), password varchar(45), role varchar(45), PRIMARY KEY (user_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_ci;
+DROP TABLE steel_future_price;
+CREATE TABLE steel_future_price (
+  price_id int(11) NOT NULL,
+  price double(10,4) DEFAULT NULL,
+  price_date date DEFAULT NULL,
+  price_code varchar(45) DEFAULT NULL,
+  PRIMARY KEY (price_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
--- src/main/java/cn/four/steel/service/SteelPriceService.java
+++ src/main/java/cn/four/steel/service/SteelPriceService.java
@@ -20,12 +20,33 @@ public class SteelPriceService {
 	
 	public void addPrice(PagePrice price){
 		String insertSql = "insert into steel_price(price_code, price, price_date, priceType) values(?,?,?,?)";
+		String futureSql = "insert into steel_future_price(price_code, price, price_date) values(?,?,?,?)";
 		List<Object> params = new ArrayList<Object>();
-		params.add(price.getPriceCode());  // 钢材规格ID
-		params.add(price.getPrice());  // 钢材价格
-		params.add(new Date());  // 价格录入日期
-		params.add(price.getPriceType());
-		jdbcTemplate.update(insertSql, params.toArray());
+		if("1".equals(price.getPriceType())){
+			params.add(price.getPriceCode());  // 钢材规格ID
+			params.add(price.getPrice());  // 钢材价格
+			params.add(new Date());  // 价格录入日期
+			params.add(price.getPriceType());
+			jdbcTemplate.update(futureSql, params.toArray());
+		} else {
+			String querySQL = "select count(*) from steel_price where price_code = ?";
+			params.add(price.getPriceCode());
+			Long cnt = jdbcTemplate.queryForObject(querySQL, params.toArray(), Long.class);
+			params.clear();
+			if(cnt == 0){
+				params.add(price.getPriceCode());  // 钢材规格ID
+				params.add(price.getPrice());  // 钢材价格
+				params.add(new Date());  // 价格录入日期
+				params.add(price.getPriceType());
+				jdbcTemplate.update(insertSql, params.toArray());
+			} else{
+				String updateSQL = "update steel_price set price = ?, price_date = ? where price_code = ?";
+				params.add(price.getPrice());  // 钢材价格
+				params.add(new Date());  // 价格录入日期
+				params.add(price.getPriceCode());  // 钢材规格ID
+				jdbcTemplate.update(updateSQL, params.toArray());
+			}
+		}
 	}
 	
 	public double findPrice(long specId){
@@ -37,7 +58,7 @@ public class SteelPriceService {
 	public List<Price> loadTodayPrice(){
 		String querySQL = "select ss.thickness, sc.steel_name, sp.price from steel_specs ss, steel_category sc, steel_price sp"
 				+ "where ss.category_id = sc.category_id and ss.price_code = sp.price_code(+)"
-				+ "and sp.price_date = ? and sp.price_type = '2' order by sc.steel_name";
+				+ "and sp.price_date = ? and sp.price_type = 2 order by sc.steel_name";
 		List<Object> params = new ArrayList<Object>();
 		params.add(new Date());
 		List<Map<String,Object>> list = jdbcTemplate.queryForList(querySQL, params.toArray());
@@ -55,9 +76,9 @@ public class SteelPriceService {
 	}
 	
 	public List<Price> loadFuturePrice(Date startDate, Date endDate){
-		String querySQL = "select ss.thickness, sc.steel_name, sp.price, sp.price_date from steel_specs ss, steel_category sc, steel_price sp"
+		String querySQL = "select ss.thickness, sc.steel_name, sp.price, sp.price_date from steel_specs ss, steel_category sc, steel_future_price sp"
 				+ "where ss.category_id = sc.category_id and ss.price_code = sp.price_code(+)"
-				+ "and sp.price_date >= ? and sp.price_date <= ? and sp.price_type = '1' order by sc.steel_name";
+				+ "and sp.price_date >= ? and sp.price_date <= ? order by sc.steel_name";
 		List<Object> params = new ArrayList<Object>();
 		params.add(startDate);
 		params.add(endDate);
